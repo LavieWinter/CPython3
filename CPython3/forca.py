8c80d5899b68a4404f04d05865240c5f7cea9531
LaveWinter <lavinia_000@outlook.com>
2023-05-30 14:01:01 -0300

    first commit

---
+++ CPython3/forca.py
@@ -0,0 +1,73 @@
+import random
+
+def jogar():
+    
+    imprime_abertura()
+    palavra_secreta = carrega_palavra()  
+    letras_acertadas = inicializa_letras_acertadas(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        
+        chute = pede_chute()
+
+        if(chute in palavra_secreta):
+           marca_chute_correto(chute, letras_acertadas, palavra_secreta)
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
+    print("Fim do jogo")
+
+
+
+#-----------------------------------------------------------------------
+
+
+
+def imprime_abertura():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+        
+def carrega_palavra():
+    arquivo = open("palavra.txt", "r")
+    palavras = []
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+    arquivo.close()
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
+    
+def inicializa_letras_acertadas(palavra):
+    return ["_" for letra in palavra]
+
+def pede_chute():
+    chute = input("Qual letra? ")
+    chute = chute.strip().upper()
+    return chute
+
+def marca_chute_correto(chute, letras_acertadas, palavra_secreta):
+    index = 0
+    for letra in palavra_secreta:
+        if(chute == letra):
+            letras_acertadas[index] = letra
+        index += 1
+    
+if(__name__ == "__main__"):
+    jogar()
